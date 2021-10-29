0d4102a825800a8c06b296bb7756899a4317723e
Annushka <aniukkan@gmail.com>
2021-10-29 18:27:21 +0300

    task_2 added

---
+++ Anna_Niukkanen_task_2/task_2_1.py
@@ -0,0 +1,15 @@
+### Task_2_1
+# Find out types of results of arithmetic operations
+
+num = 15
+
+mult = num * 3
+div = num / 3
+fdiv = num // 2
+exp = num ** 2
+
+
+print(type(mult))
+print(type(div))
+print(type(fdiv))
+print(type(exp))
