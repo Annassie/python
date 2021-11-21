94bb6eb777316a3ebe721d3c6b9da5fedb3e9333
Annushka <aniukkan@gmail.com>
2021-11-21 22:49:10 +0200

    task_2 added

--- Anna_Niukkanen_task_2/task_2_4.py
+++ Anna_Niukkanen_task_2/task_2_4.py
@@ -1,21 +1,32 @@
-### Task_2_3
-# Is given deformed list
+### Task_2_4
+# Given a list containing deformed data with positions and names of employees:
 # ['design-engineer Igor', 'general accountant MARINA', 'mechanist of the highest category nIKOLAY', 'director of aelit']
-# It's known that names are in the end of the string
-# Form and display sentences like "Hello Igor!"
-# How to get the names of employees from the list?
-# and get them in the correct form?
-# Is it possible not to create a new list?
+# It's known that the name of employee is at the end of the line/string
+# Generate and display phrases of the form "Hello Igor!"
+# How to get the names of employees from the list items?
+# and bring them to the correct form?
+# Is it possible to avoid creating a new list?
 
 # solving the problem
-# 1) get names from the list
-# 2) bring names to the correct view
-# 3) Display sentences with the names
+# 1) Get names from the list
+# 2) Bring names to the correct form
+# 3) Display phrases with the names
 # 4) NOT to create a new list
 
 
-raw_list = ['design-engineer Igor', 'general accountant MARINA', 'mechanist of the highest category nIKOLAY', 'director of aelit']
+employees = ['design-engineer Igor', 'general accountant MARINA', 'highest category mechanist nIKOLAY', 'director aelita']
+print(employees, id(employees))
 
-for item in raw_list:
-    names = raw_list.pop(::1)
-    print(names)
+names = []
+for item in employees:
+    for element in item:
+        reversed_list = item[::-1]
+        rev_name = reversed_list[:reversed_list.index(' ')]
+        name = rev_name[::-1]
+    names.append(name.title())
+print(names, id(names))
+
+for name in names:
+    greeting = f'Hi {name}!'
+    print(greeting)
+
--- Anna_Niukkanen_task_2/task_2_5.py
+++ Anna_Niukkanen_task_2/task_2_5.py
@@ -1,1 +1,43 @@
 ### Task_2_5
+
+# Create a list containing product prices (10-20 products). Exmaple [45.3, 9.4, 97, 103,5]
+
+# Display these prices as line/string, separated by common
+# The price should be displayed as <euros> euros <cents> (fro example 9 euros 40 cents)
+# Think about how to get euros and cents from the price, how to add zero, if for example it turned out 5 cents or 0 cents
+# (it should be 05 cents or 00 centes)
+# Display prices sorted in ascending order, do not create a new list
+# Create a new list containing the same prices, but sorted in descending order
+# Display prices of the five most expensive items.
+# Is it possible to display the prices of these products in ascending order with a minimum of code
+
+
+# Creating a list
+
+prices = [57.8, 46.51, 97, 103.56, 9.7, 10.1, 21.99, 150.6, 88.5, 35.6, 83.2]
+print(prices, id(prices))
+
+# A. Display these prices as line/string, separated by common
+all_prices = []
+
+for element in prices:
+    formatted_prices = "{:,.2f}".format(element)
+    del_dot = formatted_prices.replace('.', ' euros ')
+    all_prices.append(str(del_dot) + ' cents')
+    all_products = ', '.join(all_prices)
+print(all_products)
+
+
+# B. Display prices sorted in ascending order, do not create a new list
+prices.sort()
+print(prices, id(prices))
+
+# C. Create a new list containing the same prices, but sorted in descending order
+
+new_prices = sorted(prices, reverse=True)
+print(new_prices, id(new_prices))
+
+# D. Display prices of the five most expensive items.
+# Is it possible to display the prices of these products in ascending order with a minimum of code
+
+print(sorted(new_prices[:5]))
